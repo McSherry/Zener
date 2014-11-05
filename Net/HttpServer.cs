38b77ff05050fd818c5b8d5e644fa29e0f39734b
Liam McSherry <liam@synaplink.net>
2014-11-05 04:26:41 +0000

    an HTTP server that works, but isn't usable yet

--- Net/HttpServer.cs
+++ Net/HttpServer.cs
@@ -27,19 +27,48 @@ namespace SynapLink.Zener.Net
             {
                 var tcl = _listener.AcceptTcpClient();
 
-                new Thread(() => HttpRequestHandler(tcl)).Start();
+                ThreadPool.QueueUserWorkItem(HttpRequestHandler, tcl);
+                //new Thread(() => HttpRequestHandler(tcl)).Start();
             }
         }
-        private void HttpRequestHandler(TcpClient tcl)
+        private void HttpRequestHandler(object tclo)
         {
+            // Apparently, some browsers don't like servers responding
+            // in a few milliseconds. This should stop any issues.
+            // Tested against Chrome 38, Firefox 31, and IE 11. Chrome
+            // was fine with a 1ms delay, Firefox and IE were okay with
+            // 2ms. Set to 5ms for a bit of a safe zone.
+            Thread.Sleep(5);
+
+            var tcl = (TcpClient)tclo;
+   
+            tcl.NoDelay = true;
+            tcl.Client.NoDelay = true;
+
             NetworkStream ns = tcl.GetStream();
             MemoryStream ms = new MemoryStream();
-
-            var copyTask = ns.CopyToAsync(ms);
-
-            while (ns.DataAvailable) { continue; }
             
+            List<byte> buf = new List<byte>();
+            while (ns.DataAvailable)
+            {
+                buf.Add((byte)ns.ReadByte());
+            }
 
+            byte[] response = Encoding.ASCII.GetBytes(
+@"HTTP/1.1 200 OK
+Content-Type: text/plain
+
+Hello World");
+
+            ns.Write(response, 0, response.Length);
+            tcl.Close();
+
+            ms.Write(buf.ToArray(), 0, buf.Count);
+            ms.Seek(0, SeekOrigin.Begin);
+            using (var sr = new StreamReader(ms))
+            {
+                Console.WriteLine(sr.ReadLine());
+            }
         }
 
         /// <summary>
@@ -156,11 +185,16 @@ namespace SynapLink.Zener.Net
         /// Creates a new instance of the HTTP server on the specified port.
         /// </summary>
         /// <param name="port">The TCP port to listen on (1025-65535).</param>
+        /// <param name="timeout">The maximum time, in milliseconds, the server will wait for requests to be received.</param>
         public HttpServer(int port)
         {
             _port = port;
-            _listener = new TcpListener(IPAddress.Any, this.Port);
+            // Recommendation: Go directly to 127.0.0.1 and avoid the use of
+            // 'localhost'. Windows seems to have a rather slow DNS resolver,
+            // and using 'localhost' seems to add 300ms or so to response times.
+            _listener = new TcpListener(IPAddress.Loopback, this.Port);
             _listenThread = new Thread(TcpAcceptor);
+            ThreadPool.SetMinThreads(10, 10);
         }
 
         /// <summary>
