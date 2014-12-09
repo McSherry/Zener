d8520ee996b9c44a103826c3b9156acb39a8aa25
Liam McSherry <liam@synaplink.net>
2014-12-09 03:49:25 +0000

    * comment detailing route formats * method definition for Route.Match(string)

--- Core/Route.cs
+++ Core/Route.cs
@@ -29,6 +29,33 @@ namespace SynapLink.Zener.Core
     /// </summary>
     public class Route
     {
+        /*
+         * Routes use a format, based on the path requested by
+         * the client. Formats are case-insensitive, and leading
+         * and trailing forward-slashes (/) are dropped. For
+         * example, the following paths are considered identical
+         * by the router:
+         * 
+         *      /test/path/
+         *      /test/path
+         *      test/path
+         *      
+         * Additionally, routes may contain wild-card parameters.
+         * These parameters are passed to the route handler in a
+         * dictionary. Parameters are identified by a set of square
+         * brackets ([]) with the name enclosed. Examples are
+         * given below.
+         * 
+         *      user/[username]
+         *      user/[username]/profile
+         *      user/[username]/messages/[message-id]
+         * 
+         * Parameter values must not contain forward-slashes.
+         * Parameter names are converted to lowercase (for example,
+         * "[USERname]" becomes "[username]").Parameter values
+         * retain their casing.
+         */
+
         private RouteHandler _handler;
         private string _format;
         private string _name;
@@ -43,6 +70,16 @@ namespace SynapLink.Zener.Core
             _format = format.ToLower().Trim(' ', '/');
             _handler = handler;
             this.Name = this.Format;
+        }
+
+        /// <summary>
+        /// Determines whether the route matches a path.
+        /// </summary>
+        /// <param name="path">The path to test.</param>
+        /// <returns>True if the route matches the path.</returns>
+        public bool Matches(string path)
+        {
+            throw new NotImplementedException();
         }
 
         /// <summary>
