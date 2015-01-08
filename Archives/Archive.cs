4ecb1ba7bbacc86272efaf4922de68236636e780
Liam McSherry <liam@synaplink.net>
2015-01-08 03:16:56 +0000

    * add SynapLink.Zener.Archives namespace * add Archive abstract class. Provides methods for classes implementing archive interpreters/etc * add Filemark struct. Used to store a length and offset, intended use is to store the location of a file within a byte sequence of several files

---
+++ Archives/Archive.cs
@@ -0,0 +1,49 @@
+﻿/*
+ *      Copyright (c) 2014-2015, SynapLink, LLC
+ *      All Rights reserved.
+ *      
+ *      Released under BSD 3-Clause licence. See terms in
+ *      /LICENCE, or online: http://opensource.org/licenses/BSD-3-Clause
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SynapLink.Zener.Archives
+{
+    /// <summary>
+    /// Defines the interface for working with archive files.
+    /// </summary>
+    public class Archive
+    {
+        /// <summary>
+        /// The number of files in the archive.
+        /// </summary>
+        public abstract int Count { get; }
+        /// <summary>
+        /// The names of the files in the archive.
+        /// </summary>
+        public abstract IEnumerable<string> Files { get; }
+
+        /// <summary>
+        /// Retrieves a file from the archive based on its name.
+        /// </summary>
+        /// <param name="name">The name of the file to retrieve.</param>
+        /// <returns>An enumerable containing the file's contents.</returns>
+        public abstract IEnumerable<byte> GetFileByName(string name);
+        /// <summary>
+        /// Retrieves a file from the archive based on its name, and
+        /// returns it after converting it to a string with the specified
+        /// encoding.
+        /// </summary>
+        /// <param name="name">The name of the file to retrieve.</param>
+        /// <param name="encoding">The encoding to apply to the file.</param>
+        /// <returns>A string containing the encoded contents of the file.</returns>
+        public virtual string GetFileByName(string name, Encoding encoding)
+        {
+            return encoding.GetString(this.GetFileByName(name).ToArray());
+        }
+    }
+}
---
+++ Archives/Filemark.cs
@@ -0,0 +1,50 @@
+﻿/*
+ *      Copyright (c) 2014-2015, SynapLink, LLC
+ *      All Rights reserved.
+ *      
+ *      Released under BSD 3-Clause licence. See terms in
+ *      /LICENCE, or online: http://opensource.org/licenses/BSD-3-Clause
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SynapLink.Zener.Archives
+{
+    /// <summary>
+    /// Stores the location of an extracted file within a
+    /// sequence of bytes.
+    /// </summary>
+    internal struct Filemark
+    {
+        private readonly long _length, _offset;
+
+        /// <summary>
+        /// Creates a new filemark.
+        /// </summary>
+        /// <param name="length">The length of the marked file.</param>
+        /// <param name="offset">The offset of the marked file.</param>
+        public Filemark(long length, long offset)
+        {
+            _length = length;
+            _offset = offset;
+        }
+
+        /// <summary>
+        /// The length of the file, in bytes.
+        /// </summary>
+        public long Length
+        {
+            get { return _length; }
+        }
+        /// <summary>
+        /// The file's offset within the byte sequence.
+        /// </summary>
+        public long Offset
+        {
+            get { return _offset; }
+        }
+    }
+}
