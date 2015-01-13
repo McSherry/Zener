0312f209a43f135a52ffa1854d2613177bf3115d
Liam McSherry <liam@synaplink.net>
2015-01-13 22:45:47 +0000

    * replace Gzip constants with enums * rename constant ENCODING -> ISO_ENCODING

--- Archives/GzipArchive.cs
+++ Archives/GzipArchive.cs
@@ -21,35 +21,158 @@ namespace SynapLink.Zener.Archives
     public sealed class GzipArchive
         : Archive, IDisposable
     {
-        // The gzip flag bit positions.
+        private enum GzipFlags : byte
+        {
+            /// <summary>
+            /// Whether the file is likely to
+            /// contain ASCII text.
+            /// </summary>
+            AsciiText       = 0 << 0,
+            /// <summary>
+            /// Whether the archive contains a
+            /// Gzip CRC-16 header checksum.
+            /// </summary>
+            Checksum        = 1 << 0,
+            /// <summary>
+            /// Whether the archive uses optional
+            /// extra flags.
+            /// </summary>
+            OptionalExtras  = 1 << 1,
+            /// <summary>
+            /// Whether the archive contains an
+            /// original file name for the file.
+            /// </summary>
+            OriginalName    = 1 << 2,
+            /// <summary>
+            /// Whether the archive contains a
+            /// human-readable comment.
+            /// </summary>
+            Comment         = 1 << 3,
+            Reserved        = 0xF0
+        }
+        private enum GzipCompression : byte
+        {
+            /// <summary>
+            /// The DEFLATE compression method
+            /// identifier.
+            /// </summary>
+            DEFLATE         = 0x08
+        }
+        private enum GzipOs : byte
+        {
+            /// <summary>
+            /// The file is from a FAT file system, such
+            /// as is used on MS-DOS, OS/2, or NT/Win32.
+            /// </summary>
+            FAT         = 0x00,
+            /// <summary>
+            /// The file is from an Amiga OS.
+            /// </summary>
+            Amiga       = 0x01,
+            /// <summary>
+            /// The file is from VMS or OpenVMS.
+            /// </summary>
+            VMS         = 0x02,
+            /// <summary>
+            /// The file is from a UNIX system.
+            /// </summary>
+            UNIX        = 0x03,
+            /// <summary>
+            /// The file is from a VM/CMS system.
+            /// </summary>
+            VMCMS       = 0x04,
+            /// <summary>
+            /// The file is from an Atari TOS system.
+            /// </summary>
+            AtariTOS    = 0x05,
+            /// <summary>
+            /// The file is from an HPFS filesystem,
+            /// such as is used on OS/2 or NT.
+            /// </summary>
+            HPFS        = 0x06,
+            /// <summary>
+            /// The file is from a Macintosh system.
+            /// </summary>
+            Macintosh   = 0x07,
+            /// <summary>
+            /// The file is from a Z-System.
+            /// </summary>
+            ZSystem     = 0x08,
+            /// <summary>
+            /// The file is from a CP/M system.
+            /// </summary>
+            CPM         = 0x09,
+            /// <summary>
+            /// The file is from a TOPS-20 system.
+            /// </summary>
+            TOPS20      = 0x0A,
+            /// <summary>
+            /// The file is from an NTFS filesystem.
+            /// </summary>
+            NTFS        = 0x0B,
+            /// <summary>
+            /// The file is from a QDOS system.
+            /// </summary>
+            QDOS        = 0x0C,
+            /// <summary>
+            /// The file is from an Acorn RISCOS system.
+            /// </summary>
+            AcornRISCOS = 0x0D,
+            /// <summary>
+            /// The origin system is unknown.
+            /// </summary>
+            Unknown     = 0xFF
+        }
+        private enum GzipExtra : byte
+        {
+            /// <summary>
+            /// The DEFLATE algorithm used slow compression.
+            /// </summary>
+            DeflateSlow     = 0x02,
+            /// <summary>
+            /// The DEFLATE algorithm used fast compression.
+            /// </summary>
+            DeflateFast     = 0x04
+        }
+
         private const byte
-            // If set, the file is likely to be ASCII text.
-            // However, setting this bit is not mandatory.
-            FLG_FTEXT       = 0 << 0,
-            // If this is set, a CRC-16 header is present.
-            // This CRC-16 is the lower two bytes of a CRC-32
-            // of the header (excluding the checksum).
-            // Older versions of GNU zip didn't set this.
-            FLG_FHCRC       = 1 << 0,
-            // Indicates the presence of optional extra fields
-            // when set.
-            FLG_FEXTRA      = 1 << 2,
-            // Indicates when set that an ISO 8869-1 original
-            // file name is present.
-            FLG_FNAME       = 1 << 3,
-            // Indicates that an ISO 8859-1 comment is present
-            // within the file.
-            FLG_FCOMMENT    = 1 << 4,
-            FLG_RESERVED    = 0xF0;
-        private const string ENCODING = "ISO-8859-1";
+            // Gzip header is minimally 10 bytes:
+            // ID bytes, compression method identifier,
+            // flags, modification time, extra flags, and
+            // the OS identifier.
+            HEADER_LEN_MIN  = 0x0A,
+            HEADER_OFFSET   = 0x00,
+
+            FLG_OFFSET      = 0x04,
+
+            // The gzip magic number bytes. These are the first
+            // two bytes within any gzip archive file.
+            ID_1            = 0x1f,
+            ID_1_OFFSET     = 0x00,
+            ID_2            = 0x8b,
+            ID_2_OFFSET     = 0x01,
+
+            // Compression method identifier
+            CM_OFFSET       = 0x03,
+            // Modification time
+            MTIME_OFFSET    = 0x05,
+            // Optional extra flags
+            XFL_OFFSET      = 0x09,
+            // Operating system identifier
+            OS_OFFSET       = 0x0A,
+            // Optional extra length
+            XLEN_OFFSET     = 0x0B
+            ;
+        private const string ISO_ENCODING = "ISO-8859-1";
         /// <summary>
         /// The encoding to use for file names and comments.
         /// </summary>
         private static readonly Encoding IsoEncoding;
-
+        
         static GzipArchive()
         {
-            IsoEncoding = Encoding.GetEncoding(ENCODING);
+            IsoEncoding = Encoding.GetEncoding(ISO_ENCODING);
+        }
         }
 
         public GzipArchive()
