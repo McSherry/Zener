4890342982f6270c3a07677e473b5c669330ee1d
Liam McSherry <liam@synaplink.net>
2015-01-16 17:04:34 +0000

    * define class CabinetArchive

---
+++ Archives/CabinetArchive.cs
@@ -0,0 +1,23 @@
+﻿/*
+ *      Copyright (c) 2014-2015, SynapLink, LLC
+ *      All Rights reserved.
+ *      
+ *      Released under BSD 3-Clause licence. See terms in
+ *      /LICENCE, or online: http://opensource.org/licenses/BSD-3-Clause
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace SynapLink.Zener.Archives
+{
+    /// <summary>
+    /// Represents a Microsoft Cabinet/MS-CAB archive.
+    /// </summary>
+    public sealed class CabinetArchive
+        : Archive, IDisposable
+    {
+    }
+}
