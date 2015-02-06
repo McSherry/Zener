d74382d23c31fceea48a001ff20f01378ac4a204
Liam McSherry <liamj.mcsherry@googlemail.com>
2015-02-06 16:22:10 +0000

    define private enums in ZlibArchive and begin on ctor

--- Archives/ZlibArchive.cs
+++ Archives/ZlibArchive.cs
@@ -21,23 +21,94 @@ namespace McSherry.Zener.Archives
         : Archive, IDisposable
     {
         private const int
+            // The minimum length of the Zlib
+            // archive's header.
+            HEADER_MIN_LENGTH   = 0x02,
             // The offset and length of the compression
             // method and flags byte.
-            CMF_OFFSET      = 0x00,
-            CMF_LENGTH      = 0x01,
+            CMF_OFFSET          = 0x00,
+            CMF_LENGTH          = 0x01,
             // The offset and length of the flags byte.
-            FLG_OFFSET      = 0x01,
-            FLG_LENGTH      = 0x01,
+            FLG_OFFSET          = 0x01,
+            FLG_LENGTH          = 0x01,
             // The offset and length of the dictionary
             // identifier header.
-            DICTID_OFFSET   = 0x02,
-            DICTID_LENGTH   = 0x04,
+            DICTID_OFFSET       = 0x02,
+            DICTID_LENGTH       = 0x04,
             // The offset from the end of the file of
             // an Adler32 checksum of the uncompressed
             // data stored in the file.
-            ADLER32_OFFSET  = 0x04,
-            ADLER32_LENGTH  = ADLER32_OFFSET
+            ADLER32_OFFSET      = 0x04,
+            ADLER32_LENGTH      = ADLER32_OFFSET
             ;
+
+        /// <summary>
+        /// An enum containing the compression method
+        /// identifiers used within Zlib archives.
+        /// </summary>
+        private enum ZlibCompressionMethod : byte
+        {
+            /// <summary>
+            /// The bit mask used to extract the
+            /// relevant bits from the CMF byte.
+            /// </summary>
+            BitMask     = 0x0F,
+            /// <summary>
+            /// The DEFLATE compression algorithm.
+            /// </summary>
+            DEFLATE     = 0x08
+        }
+        /// <summary>
+        /// An enum containing values relevant to the
+        /// compression info field in Zlib archives.
+        /// </summary>
+        private enum ZlibCompressionInfo : byte
+        {
+            /// <summary>
+            /// The bit mask for extracting the bits of
+            /// the compression information field from
+            /// the CMF byte.
+            /// </summary>
+            BitMask     = 0xF0
+        }
+        /// <summary>
+        /// An enum containing flags used within the
+        /// Zlib archvie flags byte.
+        /// </summary>
+        private enum ZlibFlags : byte
+        {
+            /// <summary>
+            /// A bit mask used to extract the check bits
+            /// from the FLG field.
+            /// </summary>
+            CheckBits               = 0x1F,
+            /// <summary>
+            /// The flag that indicates whether the DICTID
+            /// field is present within the archive.
+            /// </summary>
+            DictionaryIdPresent     = 0x20,
+            /// <summary>
+            /// The bit mask used to extract the compression
+            /// level information from the FLG field.
+            /// </summary>
+            CompressionLevelBits    = 0xC0,
+            /// <summary>
+            /// The fastest DEFLATE compression level.
+            /// </summary>
+            FastestCompression      = 0x00,
+            /// <summary>
+            /// The fast DEFLATE compression level.
+            /// </summary>
+            FastCompression         = 0x40,
+            /// <summary>
+            /// The default DEFLATE compression level.
+            /// </summary>
+            DefaultCompression      = 0x80,
+            /// <summary>
+            /// The best but slowest DEFLATE compression level.
+            /// </summary>
+            MaximumCompression      = 0xC0
+        }
 
         // The name of the file stored within
         // the archive.
@@ -45,10 +116,53 @@ namespace McSherry.Zener.Archives
         // The uncompressed data stored within
         // the archive.
         private byte[] _data;
+        // The Adler32 checksum stored in the
+        // archive's trailer.
+        private uint _checksum;
+        // The compression method identifier from the archive's
+        // headers.
+        private ZlibCompressionMethod _compMethod;
+        // The compression method information from the archive's
+        // headers.
+        private ZlibCompressionInfo _compInfo;
+        // The flags from the archive's headers.
+        private ZlibFlags _flags;
 
+        /// <summary>
+        /// Creates a new ZlibArchive.
+        /// </summary>
+        /// <param name="stream">The stream containing the archive's bytes.</param>
+        /// <exception cref="System.ArgumentException">
+        ///     Thrown when the provided stream does not support reading
+        ///     or does not support seeking.
+        /// </exception>
+        /// <exception cref="System.ArgumentNullException">
+        ///     Thrown when the provided stream is null.
+        /// </exception>
         public ZlibArchive(Stream stream)
         {
+            if (stream == null)
+            {
+                throw new ArgumentNullException(
+                    "The provided stream cannot be null."
+                    );
+            }
 
+            if (!stream.CanRead || !stream.CanSeek)
+            {
+                throw new ArgumentException(
+                    "The provided stream must support reading/seeking."
+                    );
+            }
+
+            byte[] hdrBuf = new byte[HEADER_MIN_LENGTH];
+            stream.Read(hdrBuf, 0, HEADER_MIN_LENGTH);
+
+            byte cmf = hdrBuf[CMF_OFFSET],
+                flg = hdrBuf[FLG_OFFSET];
+            _compMethod = (ZlibCompressionMethod)(cmf & (byte)ZlibCompressionMethod.BitMask);
+            _compInfo = (ZlibCompressionInfo)(cmf & (byte)ZlibCompressionInfo.BitMask);
+            _flags = (ZlibFlags)flg;
         }
 
         /// <summary>
