296c9e8a9a2a7c3e94e70c167fc745cf1103865f
Liam McSherry <liamj.mcsherry@googlemail.com>
2015-02-09 16:00:03 +0000

    preliminary Basic authentication implementation (#30)

--- Net/HttpResponse.cs
+++ Net/HttpResponse.cs
@@ -246,13 +246,20 @@ namespace McSherry.Zener.Net
             HDR_SERVER          = "Server",
             HDR_CONTENTTYPE     = "Content-Type",
             HDR_DATE            = "Date",
+            HDR_CLAUTH          = "Authorization",
+            HDR_SVAUTH          = "WWW-Authenticate",
 
             HDRF_CHUNKEDXFER    = "Chunked",
             HDRF_SERVER         = "Zener/{0}",
             HDRF_CONTENTTYPE    = "text/html",
+            HDRF_SVAUTH_BASIC   = "Basic realm=\"{0}\"",
+            HDRF_CLAUTH_STBSC   = "Basic ",
 
             HTTP_VERSION        = "1.1",
-            HTTP_NEWLINE        = "\r\n"
+            HTTP_NEWLINE        = "\r\n",
+            // Invalid characters in Basic authentication
+            // "realm" field values.
+            HTTP_SVBASIC_RLMINV = "\"\r\n"
             ;
         internal const int
             TX_BUFFER_SIZE      = 16384
@@ -753,6 +760,135 @@ namespace McSherry.Zener.Net
             formatBuilder.Append(HTTP_NEWLINE);
 
             this.Write(value: formatBuilder.ToString());
+        }
+
+        /// <summary>
+        /// Authenticates the client using HTTP Basic authentication.
+        /// If authentication fails, responds to the client requesting
+        /// credentials and closes the response.
+        /// </summary>
+        /// <param name="username">
+        ///     The username to be required for successful authentication.
+        /// </param>
+        /// <param name="password">
+        ///     The password to be required for successful authentication.
+        /// </param>
+        /// <param name="realm">
+        ///     The name of the protection space these credentials are
+        ///     used in.
+        /// </param>
+        /// <returns>
+        ///     True if the authentication was successful. If the method
+        ///     returns False, the HttpResponse has been closed.
+        /// </returns>
+        /// <exception cref="System.InvalidOperationException">
+        ///     Thrown when the method is called after the headers or
+        ///     sent, or when it is called after the connection has been
+        ///     closed.
+        /// </exception>
+        public bool BasicAuthenticate(
+            string username, string password,
+            string realm = null
+            )
+        {
+            _CheckClosed();
+            _CheckResponding();
+
+            this.BufferOutput = true;
+
+            var hdr = _request.Headers[HDR_CLAUTH].DefaultIfEmpty(null).First();
+            // If hdr is null, it means that the client hasn't sent an
+            // Authorization header. This means we now need to respond with
+            // a 401 and request authorization.
+            if (hdr == null)
+            {
+                if (String.IsNullOrEmpty(realm))
+                {
+                    realm = _request.Path;
+                }
+
+                realm = new string(realm
+                    .Where(c => !HTTP_SVBASIC_RLMINV.Contains(c))
+                    .ToArray()
+                    );
+
+                this.StatusCode = HttpStatus.Unauthorized;
+                this.Headers.Add(
+                    fieldName:  HDR_SVAUTH,
+                    fieldValue: String.Format(HDRF_SVAUTH_BASIC, realm)
+                    );
+
+                // We've sent out WWW-Authentication header, and we don't
+                // want user code to be able to send any data. Close the
+                // response.
+                this.Close();
+                // Authentication of the client did not succeed. Return false.
+                return false;
+            }
+            else
+            {
+                if (!hdr.Value.StartsWith(HDRF_CLAUTH_STBSC))
+                {
+                    // If the Authorization header doesn't start
+                    // with the string "Basic ", it is invalid. We
+                    // remove the Authorization header and recurse.
+                    // This results in the above branch being executed
+                    // and the sending of a WWW-Authenticate header to
+                    // the client.
+                    _request.Headers.IsReadOnly = false;
+                    _request.Headers.Remove(hdr.Field);
+                    _request.Headers.IsReadOnly = true;
+
+                    this.BasicAuthenticate(username, password, realm);
+                }
+
+                // The remainder of the Authorization header should be the
+                // Base64 string containing the user's authentication details.
+                string authStr = hdr.Value.Substring(HDRF_CLAUTH_STBSC.Length);
+
+                // We'll check that the string is valid Base64. This lets us provide
+                // better error messages: 400 for invalid data, 401 for invalid
+                // credentials.
+                try
+                {
+                    Rfc2045.Base64Decode(authStr);
+                }
+                catch (ArgumentException aex)
+                {
+                    throw new HttpRequestException(
+                        _request,
+                        "The client's Authorization header contains an invalid Base64 string.",
+                        aex
+                        );
+                }
+                catch (InvalidOperationException ioex)
+                {
+                    throw new HttpRequestException(
+                        _request,
+                        "The client's Authorization header contains an invalid Base64 string.",
+                        ioex
+                        );
+                }
+
+                string creds = Rfc2045.Base64Encode(String.Format("{0}:{1}", username, password));
+                if (!creds.Equals(authStr))
+                {
+                    // If the Authorization header doesn't contain
+                    // valid credentials, it cannot authorise the client.
+                    // We remove the Authorization header and recurse.
+                    // This results in the above branch being executed
+                    // and the sending of a WWW-Authenticate header to
+                    // the client.
+                    _request.Headers.IsReadOnly = false;
+                    _request.Headers.Remove(hdr.Field);
+                    _request.Headers.IsReadOnly = true;
+
+                    this.BasicAuthenticate(username, password, realm);
+                }
+            }
+
+            // Authentication of the client succeeded. Return true.
+            return true;
         }
 
         /// <summary>
