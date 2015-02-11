65ee750b0d4f2563604e232cf012097c39d5037d
Liam McSherry <liamj.mcsherry@googlemail.com>
2015-02-11 13:42:39 +0000

    add CRC-32 generation to Rfc1952 (untested)

--- Core/Rfc1952.cs
+++ Core/Rfc1952.cs
@@ -9,6 +9,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.IO;
 
 namespace McSherry.Zener.Core
 {
@@ -18,25 +19,26 @@ namespace McSherry.Zener.Core
     public static class Rfc1952
     {
         #region CRC-32 lookup table
-        private static readonly ulong[] CRC_TABLE;
-        private static void _GenerateCrc32Lookup(out ulong[] array)
+        private const uint CRC_INIT = 0x00000000;
+        private static readonly uint[] CRC_TABLE;
+        private static void _GenerateCrc32Lookup(out uint[] array)
         {
             /* This function adapted from the example code in
              * RFC 1952's appendix 8. This function (_GenerateCrc32Lookup)
              * is released in to the public domain.
              */
-            array = new ulong[256];
+            array = new uint[256];
 
-            ulong c;
+            uint c;
             for (int n = 0; n < array.Length; n++)
             {
-                c = (ulong)n;
+                c = (uint)n;
 
                 for (int k = 0; k < 8; k++)
                 {
                     if ((c & 1) == 1)
                     {
-                        c = 0xEDB88320L ^ (c >> 1);
+                        c = 0xEDB88320U ^ (c >> 1);
                     }
                     else
                     {
@@ -52,6 +54,55 @@ namespace McSherry.Zener.Core
         static Rfc1952()
         {
             _GenerateCrc32Lookup(out CRC_TABLE);
+        }
+
+        /// <summary>
+        /// Generates an RFC 1952-compliant CRC-32 checksum
+        /// from the provided stream.
+        /// </summary>
+        /// <param name="stream">The stream to generate a checksum from.</param>
+        /// <returns>
+        ///     An unsigned 32-bit integer containing the generated
+        ///     checksum.
+        /// </returns>
+        /// <exception cref="System.ArgumentNullException">
+        ///     Thrown when the provided stream is null.
+        /// </exception>
+        /// <exception cref="System.ArgumentException">
+        ///     Thrown when the provided stream does not
+        ///     support reading.
+        /// </exception>
+        public static uint GenerateCrc32(Stream stream)
+        {
+            /* This function adapted from the example code in
+             * RFC 1952's appendix 8. This function (GenerateCrc32)
+             * is released in to the public domain.
+             */
+
+            if (stream == null)
+            {
+                throw new ArgumentNullException(
+                    "The provided stream must not be null."
+                    );
+            }
+
+            if (!stream.CanRead)
+            {
+                throw new ArgumentException(
+                    "The provided stream must support reading."
+                    );
+            }
+
+            uint crc = 0,
+                c = crc ^ 0xFFFFFFFF;
+
+            int b = 0;
+            while ((b = stream.ReadByte()) != -1)
+            {
+                c = CRC_TABLE[(c ^ (byte)b) & 0xFF] ^ (c >> 8);
+            }
+
+            return c ^ 0xFFFFFFFF;
         }
     }
 }
